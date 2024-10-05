ee2d0b94085892ce0945d40b52c1962c08d52248
girija <girijadalvi04@gmail.com>
2024-10-05 15:33:21 +0530

    first commit

---
+++ basicop.py
@@ -0,0 +1,65 @@
+#arithmatic operations
+a=10
+b=20
+print('a+b=',a+b)
+print('a-b=',a-b)
+print('a*b=',a*b)
+print('a/b=',a/b)
+print('a^b=',a^b)
+print('a**b=',a**b)
+print('a%b=',a%b)
+print('a//b=',a//b)
+
+
+#user input 
+a=int(input('enter a:'))
+b=int(input('enter b:'))
+print('a+b=',a+b)
+print('a-b=',a-b)
+print('a*b=',a*b)
+print('a/b=',a/b)
+print('a^b=',a^b)
+print('a**b=',a**b)
+print('a%b=',a%b)
+print('a//b=',a//b)
+
+
+a=int(input('enter a:'))
+b=int(input('enter b:'))
+print ('a and b=',a and b)
+print ('a or b=',a or b)
+print ('a not=', not a)
+
+a=int(input('enter a:'))
+b=int(input('enter b:'))
+print('a&b=',a & b)
+print('a|b=',a | b)
+print('~a=',~a)
+print('~b=',~b)
+print('a^b=',a ^ b)
+print('a>>b=',a >> b)
+print('a<<b=',a << b)
+
+
+a=int(input('enter a:'))
+b=int(input('enter b:'))
+print('a>b=',a > b)
+print('a< b=',a < b)
+print('a>=b ',a >= b)
+print('a<=b ',a<=b)
+print('a=b:',a == b)
+print('a!=b=',a != b)
+
+L1=["APPLE","MANGO","CHERRY"]
+L2=["ORANGE","GREEN"]
+L3=L1
+print("APPLE" in L1)
+print("ORANGE" in L1)
+print("APPLE" not in L1)
+print("ORANGE" not in L1)
+print(L1 is L2)
+print(L1 is not L2)
+print(L1 is L3)
+print(L2 is L3)
+
+
